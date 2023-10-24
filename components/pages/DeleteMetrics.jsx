36c8dbf1d9ef24980f35eb3791c3970504ef552a
Tanner-Singletary <80844006+Tanner-Singletary@users.noreply.github.com>
2023-10-23 20:35:35 -0700

    Update note to prompt users to go to home page themselves

--- components/pages/DeleteMetrics.jsx
+++ components/pages/DeleteMetrics.jsx
@@ -26,7 +26,8 @@ export default function DeleteMetrics(props) {
             .delete()
             .eq('user', props.session.user.id)
             .in('name', metricNamesToDelete)
-        alert("Deletion submitted, returning to home page.");
+        alert("Deletion submitted, return to home page to see remaining metrics.");
+        // BUGFIX: This seems to work in ios/mobile but not web
         props.setActivePage("home");
     }
 
