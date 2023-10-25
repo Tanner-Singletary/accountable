5b81012214c34ff16faae14335107eee75a41d91
Tanner-Singletary <80844006+Tanner-Singletary@users.noreply.github.com>
2023-10-24 19:51:33 -0700

    metricLogs query working

--- components/Main.jsx
+++ components/Main.jsx
@@ -7,12 +7,15 @@ import alert from '../lib/alertPolyfill';
 export default function Main({session}) {
     const [todayScore, setTodayScore] = useState(0);
     const [activePage, setActivePage] = useState("home");
-    const [loading, setLoading] = useState(false);
+    const [loadingMetrics, setLoadingMetrics] = useState(false);
+    const [loadingMetricLogs, setLoadingMetricLogs] = useState(false);
     const [metrics, setMetrics] = useState([]);
-
+    const [metricLogs, setMetricLogs] = useState([]);
 
     useEffect(() => {
-        getMetrics()
+        getMetrics();
+        getMetricLogs();
+
         /* activePage as dependency allows for refresh to display newly added metrics
         when navigating back to the home page, with the drawback that it unnecessarily
         does a GET call when changing to other pages (i.e. add metrics). Adding the
@@ -28,13 +31,13 @@ export default function Main({session}) {
 
     async function getMetrics() {
         try {
-          setLoading(true)
+          setLoadingMetrics(true)
           const { data, error, status } = await supabase
             .from('metrics')
             .select(`id,name,category`)
-            .eq('user', session.user.id)
+            .eq('user', session.user.id);
           if (error && status !== 406) {
-            throw error
+            throw error;
           }
     
           if (data) {
@@ -45,12 +48,35 @@ export default function Main({session}) {
             alert(error.message);
           }
         } finally {
-          setLoading(false);
+          setLoadingMetrics(false);
+        }
+      }
+
+      async function getMetricLogs() {
+        try {
+          setLoadingMetricLogs(true)
+          const { data, error, status } = await supabase
+            .from('metric_logs')
+            .select(`id,created_at,user_id,metric_id`)
+            .eq('user_id', session.user.id);
+          if (error && status !== 406) {
+            throw error;
+          }
+    
+          if (data) {
+            setMetricLogs(data);
+          }
+        } catch (error) {
+          if (error instanceof Error) {
+            alert(error.message);
+          }
+        } finally {
+          setLoadingMetricLogs(false);
         }
       }
 
     return (
-        loading ? <Text>Loading...</Text> :
+        loadingMetrics || loadingMetricLogs ? <Text>Loading...</Text> :
         <UserInterface
             todayScore={todayScore}
             updateTodayScore={updateTodayScore}
